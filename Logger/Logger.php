ccdaa91fdf1837389641fa5155789cac1a65e188
Raffaele Sarracino <raffaele56t@gmail.com>
2021-01-01 00:11:28 +0100

    Added methods for logger

--- Logger/Logger.php
+++ Logger/Logger.php
@@ -15,14 +15,24 @@ class Logger extends AbstractLogger
 
     private $logPath;
 
+    protected $appendToFile = true;
+
+    protected $json = true;
+
     /**
      * Logger constructor.
      *
      * @param string|null $path the path of the log files. If it is null logs are not generated
      *                          If it is not null, check that path is a dir with write permissions
+     * @param bool $appendToFile
+     * @param bool $json
      * @throws \Exception
      */
-    private function __construct(string $path = null) {
+    private function __construct(string $path = null, bool $appendToFile = false, bool $json = false) {
+
+        $this->appendToFile = $appendToFile;
+        $this->json = $json;
+
         if(!is_null($path)) {
             $this->setPath($path);
         }
@@ -61,9 +71,66 @@ class Logger extends AbstractLogger
         return $this->logPath;
     }
 
+    /**
+     * Generate a log
+     *
+     * @param mixed $level
+     * @param string $message
+     * @param array $context
+     */
     public function log($level, $message, array $context = array())
     {
-        // TODO: Implement log() method.
+        $log = $this->generateLog($level, $message, $context);
+
+        if($this->json) {
+            $data = $log->jsonSerialize();
+        }
+        else {
+            $data = $log->toString();
+        }
+
+        $this->writeLog($data);
+    }
+
+    /**
+     * Generate the log data structure
+     *
+     * @param $level
+     * @param $message
+     * @param array $context
+     * @return Log
+     */
+    private function generateLog($level, $message, array $context) {
+        //generate base log info
+        return new Log((new \DateTime()), $level, $message, $context);
+    }
+
+    /**
+     * Write the data on the log file
+     *
+     * @param string $data
+     * @return bool
+     */
+    private function writeLog(string $data) {
+        $file = $this->getPath().'/logger.log';
+        try {
+
+            if($this->appendToFile) {
+                //append data at end of file
+                $written = file_put_contents($file, $data.PHP_EOL , FILE_APPEND | LOCK_EX);
+            }
+            else {
+                $oldData = (file_exists($file) ? file_get_contents($file) : '');
+                //append data to start of file
+                $written = file_put_contents($file, $data.PHP_EOL.$oldData, LOCK_EX);
+            }
+
+        }
+        catch (\Exception $e) {
+            $written = false;
+        }
+
+        return ($written !== false && $written > 0);
     }
 
     /**
