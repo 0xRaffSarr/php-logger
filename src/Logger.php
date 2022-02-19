b88783da7c22a073459dcae80c6f8824bf833174
Raffaele Sarracino <contacts@raffaelesarracino.it>
2022-02-19 15:46:09 +0100

    Removed import

--- src/Logger.php
+++ src/Logger.php
@@ -12,7 +12,6 @@
 namespace PhpLogger;
 
 use PhpLogger\Log\Log;
-use PhpLogger\Reader\ReaderInterface;
 use PhpLogger\Writer\WriterFactory;
 use PhpLogger\Writer\WriterInterface;
 use Psr\Log\AbstractLogger;
